c6ecf0508200fdafdf9fafc220dc5d3f6a7db6ee
Roshan Chaudhary <87389059+Roshanchau@users.noreply.github.com>
2024-02-19 00:46:40 +0545

    ecommerce admin panel setup and shadcn usage

---
+++ app/(root)/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+
+export default function Page() {
+  return (
+    <Button 
+    variant="destructive"
+    size="lg"
+    >
+    click me</Button>
+  );
+}
