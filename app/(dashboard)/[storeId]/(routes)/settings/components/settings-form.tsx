19e954a28fb36fccc833d6155a0dc695dff24ef1
Roshan Chaudhary <87389059+Roshanchau@users.noreply.github.com>
2024-04-25 23:29:24 +0545

    create patch and delete api routes for the settings form

--- app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.tsx
+++ app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.tsx
@@ -1,82 +1,107 @@
-'use client'
+"use client";
 
 import { useState } from "react";
 import { useForm } from "react-hook-form";
-import { Store } from "@prisma/client"
+import { Store } from "@prisma/client";
 import { Trash } from "lucide-react";
 import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
+import toast from "react-hot-toast";
+import axios from "axios";
+import { useParams, useRouter } from "next/navigation";
 
 import { Button } from "@/components/ui/button";
 import { Heading } from "@/components/ui/heading";
 import { Separator } from "@/components/ui/separator";
-import { Form, FormControl, FormField, FormItem, FormLabel } from "@/components/ui/form";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 
 interface SettingFormProps {
-    initailData: Store;
+  initailData: Store;
 }
 
-const formSchema= z.object({
-  name: z.string().min(1)
+const formSchema = z.object({
+  name: z.string().min(1),
 });
 
-type SettingFormValues= z.infer<typeof formSchema>;
+type SettingFormValues = z.infer<typeof formSchema>;
 
-const SettingsForm: React.FC<SettingFormProps> = ({
-    initailData
-}) => {
-  const[open, setOpen]=useState(false);
-  const[loading, setLoading]=useState(false);
+const SettingsForm: React.FC<SettingFormProps> = ({ initailData }) => {
+  const [open, setOpen] = useState(false);
+  const [loading, setLoading] = useState(false);
 
-  const form=useForm<SettingFormValues>({
+  const params=useParams();
+  const router=useRouter();
+
+  const form = useForm<SettingFormValues>({
     resolver: zodResolver(formSchema),
-    defaultValues: initailData
+    defaultValues: initailData,
   });
 
-  const onSubmit=async(data: SettingFormValues)=>{
-    console.log(data);
-  }
+  const onSubmit = async (data: SettingFormValues) => {
+    try{
+      await axios.patch(`/api/stores/${params.storeId}` , data);
+      router.refresh();
+      toast.success("Store updated successfully");
+    }catch(error){
+      toast.error("something went wrong")
+    }finally{
+      setLoading(false);
+    }
+  };
 
   return (
-  <>
-    <div className="flex items-center justify-between">
-        <Heading
-            title="Settings"
-            description="Manage store prefrences"
-        />
-        <Button
-          variant="destructive"
-          size="icon"
-          onClick={()=>[]}
+    <>
+      <div className="flex items-center justify-between">
+        <Heading title="Settings" description="Manage store prefrences" />
+        <Button 
+          disabled={loading}
+          variant="destructive" 
+          size="icon" 
+          onClick={() => setOpen(true)}
         >
-          <Trash className="h-4 w-4"/>
+          <Trash className="h-4 w-4" />
         </Button>
-    </div>
-    <Separator/>
-    <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8 w-full">
-        <div className="grid grid-cols-3 gap-8">
-          <FormField
-            control={form.control}
-            name="name"
-            render={({field})=>(
-              <FormItem>
-                <FormLabel>Name</FormLabel>
-                <FormControl>
-                  <Input disabled={loading} placeholder="Store name" {...field}/>
-                </FormControl>
-              </FormItem>
-            )}
-          />
-        </div>
-        <Button disabled={loading} className="ml-auto" type="submit">
-          Save Changes
-        </Button>
-      </form>
-    </Form>
-  </>
-  )
-}
+      </div>
+      <Separator />
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className="space-y-8 w-full"
+        >
+          <div className="grid grid-cols-3 gap-8">
+            <FormField
+              control={form.control}
+              name="name"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Name</FormLabel>
+                  <FormControl>
+                    <Input
+                      disabled={loading}
+                      placeholder="Store name"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </div>
+          <Button disabled={loading} className="ml-auto" type="submit">
+            Save Changes
+          </Button>
+        </form>
+      </Form>
+    </>
+  );
+};
 
-export default SettingsForm
+export default SettingsForm;
